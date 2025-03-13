9344fd79aea4ff65094035284c3cbd98f5f5ec00
Andrew Huang <vivatv.it0001@gmail.com>
2025-03-13 09:53:20 +0800

    [Feature] Create openCollectionView Button

--- CollectionIssueDemo/ViewController.swift
+++ CollectionIssueDemo/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func openCollectionView(_ sender: UIButton) {
+        print("我進來了")
+    }
 }
 
